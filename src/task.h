42e1a120cc9b84d88dd888b61c0a911fa4e4a9e4
Milad Esfandiyari <miladisjobs@gmail.com>
2020-07-06 18:33:46 +0430

    change

--- src/task.h
+++ src/task.h
@@ -334,6 +334,13 @@ is used in assert() statements. */
                             void * const pvParameters,
                             UBaseType_t uxPriority,
                             TaskHandle_t * const pxCreatedTask ) PRIVILEGED_FUNCTION;
+    BaseType_t xTaskPeriodicCreate( TaskFunction_t pxTaskCode,
+                            const char * const pcName,  /*lint !e971 Unqualified char types are allowed for strings and single characters only. */
+                            const configSTACK_DEPTH_TYPE usStackDepth,
+                            void * const pvParameters,
+                            UBaseType_t uxPriority,
+                            TaskHandle_t * const pxCreatedTask,
+                            TickType_t period ) PRIVILEGED_FUNCTION  ;                      
 #endif
 
 /**
